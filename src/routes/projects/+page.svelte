ec7d2ee7e5631f34305310178cfd1c9e1b6158f4
Gregor <gregorpodjed@yahoo.com>
2024-11-27 19:45:23 +0100

    Added projects, added a new thing we made at 404

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ClickableButton from '$lib/ClickableButton.svelte';
+</script>
+
+<h1>My projects</h1>
+<p>Stuff I made at a coding class</p>
+<ul>
+	<li>
+		<ClickableButton url="/projects/to-do" label="To-do list" />
+	</li>
+</ul>
