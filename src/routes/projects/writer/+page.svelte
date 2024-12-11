bfe1115de6233b6ec7ff32ee2b76a0f23981ec3c
Gregor <gregor@gregtech.eu>
2024-12-11 21:09:14 +0100

    Added a new project with some options to format text

--- src/routes/projects/writer/+page.svelte
+++ src/routes/projects/writer/+page.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
 	import PageTitle from '$lib/PageTitle.svelte';
-	import ResultBox from '$lib/ResultBox.svelte';
+	import { Copy } from 'lucide-svelte';
 	let text: string = '';
-	let maxlength = 300;
-	import crypto from 'crypto';
-	//const randomString = crypto.randomBytes(1).toString('hex');
+
 	function color() {
 		return '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
 	}
+	let i;
+	function firstLetterUppercase(input) {
+		var res = '';
+		for (var i = 0; i < input.length; i++) {
+			// Make every first character uppercase and second character lowercase alternately
+			res += i % 2 === 0 ? input.charAt(i).toUpperCase() : input.charAt(i).toLowerCase();
+		}
+		return res;
+	}
+	import { copy } from 'svelte-copy';
 </script>
 
 <PageTitle title="Writer" />
 <div class=" overflow-auto p-4 flex flex-col text-black items-center justify-center">
-	<textarea {maxlength} bind:value={text} class="w-1/2 border-2 p-2 border-violet-600 rounded-lg"
-	></textarea>
-	<div class="grid grid-cols-2 gap-4">
+	<textarea bind:value={text} class="w-1/2 border-2 p-2 border-violet-600 rounded-lg"></textarea>
+	<div class="grid grid-cols-4 gap-4">
+		<button class="pill-button" on:click={() => (text = firstLetterUppercase(text))}>MoCkIfY</button
+		>
 		<button class="pill-button" on:click={() => (text = text.toUpperCase())}>UPPER</button>
 		<button class="pill-button" on:click={() => (text = text.toLowerCase())}>lower</button>
-		<p>{text.length}/{maxlength}</p>
+		<button class="pill-button flex" use:copy={text}>Copy text<Copy class="ml-2" /></button>
 	</div>
 	<div class="flex flex-wrap w-1/2 border-2 m-2">
 		{#each text.split(' ') as word}
