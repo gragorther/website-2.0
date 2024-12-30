275550a8eebee049ff8b8fe593ca8c4f14f55629
Gregor <gregor@gregtech.eu>
2024-12-30 21:06:52 +0100

    Made the text of the nav buttons white even when they are hovered on

--- src/lib/NavButton.svelte
+++ src/lib/NavButton.svelte
@@ -8,6 +8,6 @@
 
 <a
 	{href}
-	class="rounded-lg bg-purple-400 text-xl p-2 text-center hover:bg-purple-700 text-white flex-grow mr-2 sm:text-lg sm:p-3 lg:text-xl lg:p-0"
+	class="rounded-lg bg-purple-400 text-xl p-2 text-center hover:bg-purple-700 text-white flex-grow mr-2 sm:text-lg sm:p-3 lg:text-xl lg:p-0 hover:text-white"
 	>{@render children?.()}</a
 >
